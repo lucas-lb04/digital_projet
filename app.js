63367be4af3af2a0462b0ca02097d5572409ded3
lucas-lb04 <lucas.bui@efrei.net>
2025-06-04 13:30:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 4000; // changer le port pour éviter le conflit avec Grafana
-const jsonData = require("./data/data.json"); // assure-toi que le fichier existe
+const port = 4000; 
+const jsonData = require("./data/data.json"); 
 
-app.use(express.static("public")); // dossiers statiques dans /public
-
+app.use(express.static("public")); 
 app.get("/api/data", (req, res) => {
     res.json(jsonData);
 });
 
-// Route racine pour servir index.html
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
